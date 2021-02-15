3d8eaa75a5661ed5891c7937b4192cdcc8756826
Muhammad Rydwan <rydwan.dev@gmail.com>
2021-02-15 13:48:02 +0700

    Fix type on recipe.action.ts

--- src/app/recipes/store/recipe.action.ts
+++ src/app/recipes/store/recipe.action.ts
@@ -3,7 +3,7 @@ import { Recipe } from '../recipe.model';
 
 export const SET_RECIPES = '[Recipes] Set Recipes';
 export const FETCH_RECIPES = '[Recipes] Fetch Recipes';
-export const ADD_RECIPE = '[Recipe] Add Recipe';
+export const ADD_RECIPE = '[Recipes] Add Recipe';
 export const UPDATE_RECIPE = '[Recipe] Update Recipe';
 export const DELETE_RECIPE = '[Recipe] Delete Recipe';
 export const STORE_RECIPES = '[Recipe] Store Recipes';
